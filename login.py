4ed8fbf4752f8c2f2fa57d74dfb115a92cf6e945
megatrisnaw <megatrisnaww@gmail.com>
2026-01-22 07:21:25 +0700

    appium commit

---
+++ login.py
@@ -0,0 +1,62 @@
+from appium import webdriver
+from appium.options.android import UiAutomator2Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+options = UiAutomator2Options()
+options.platform_name = "Android"
+options.device_name = "A54-Mega"
+options.app_package = "com.starwarsapp"
+options.app_activity = "com.starwarsapp.MainActivity"
+options.automation_name = "UiAutomator2"
+options.no_reset = True  
+
+# START DRIVER
+driver = webdriver.Remote(
+    "http://127.0.0.1:4723",  
+    options=options
+)
+
+wait = WebDriverWait(driver, 15)
+
+# EMAIL FIELD
+email_field = wait.until(
+    EC.presence_of_element_located(
+        (
+            "xpath",
+            "//android.widget.EditText[@hint='E-mail address']"
+        )
+    )
+)
+email_field.click()
+email_field.clear()
+email_field.send_keys("user1@gmail.com")
+
+# PASSWORD FIELD
+password_field = wait.until(
+    EC.presence_of_element_located(
+        (
+            "xpath",
+            "//android.widget.EditText[@hint='Password']"
+        )
+    )
+)
+password_field.click()
+password_field.clear()
+password_field.send_keys("Mega2017.")
+
+
+# LOGIN BUTTON
+login_button = wait.until(
+    EC.element_to_be_clickable(
+        (
+            "xpath",
+            "//android.view.ViewGroup[@content-desc='LOGIN']"
+        )
+    )
+)
+login_button.click()
+time.sleep(5)
+
+# driver.quit()  
